86847e782795518829e3a7afeb7676786c1bb3d0
Richard Tekula <barspin4499@gmail.com>
2025-01-22 10:19:07 +0100

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import OneProduct from './pages/NotFound';
 import NotFound from './pages/OneProduct';
 import Navigation from './components/Navigation'
 import Basket from './pages/Basket';
@@ -11,7 +10,6 @@ const App = () => {
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} /> 
-        <Route path="/pdoduct/:id" element={<OneProduct />} />
         <Route path="/basket" element={<Basket />} />
         <Route path="*" element={<NotFound />} /> 
       </Routes>
--- src/pages/OneProduct.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const OneProduct = () => {
-  return (
-    <div>OneProduct</div>
-  )
-}
-
-export default OneProduct
